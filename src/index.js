0ae1c108732dc2292b51203a0e9daa664626efb5
ain-odin <ain.odintseva@gmail.com>
2023-01-09 14:34:31 +0600

    feat: implement function that reverse digits of this number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
-}
+    let arr = String(n).split('');
+    let newArr = arr.filter(nums => nums != "-");
+    let str = newArr.reverse().join('');
+    return Number(str);
+};
